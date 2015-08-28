341a23f02539b0b2c2e4c2123b24ffcdac37f409
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-08-28 10:14:49 -0700

    Update commit bash script.

--- bash_scripts/commit.sh
+++ bash_scripts/commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STORY=`git symbolic-ref --short HEAD | tr '-' '\n' | tail -n2 | head -n1 | tr "[:lower:]" "[:upper:]"`
+STORY=`git symbolic-ref --short HEAD | tr '-' '\n' | head -n1 | tr "[:lower:]" "[:upper:]"`
 TASK=`git symbolic-ref --short HEAD | tr '-' '\n' | tail -n1 | tr "[:lower:]" "[:upper:]"`
 
 git commit -m "$STORY - $TASK - $1."
