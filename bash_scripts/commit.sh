4bac8302da5bb78135023a38982a2d7b5f1a6893
Paul Statezny <paulstatezny@gmail.com>
2016-08-08 15:27:12 -0700

    Update commit.sh

--- bash_scripts/commit.sh
+++ bash_scripts/commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NUMBER=`git symbolic-ref --short HEAD | tr '-' '\n' | head -n1 | tr "[:lower:]" "[:upper:]"`
+NUMBER=`git symbolic-ref --short HEAD | tr '-' '\n' | tail -n1 | tr "[:lower:]" "[:upper:]"`
 
-git commit -m "$1. (#$NUMBER)"
+git commit -m "$1 (#$NUMBER)"
