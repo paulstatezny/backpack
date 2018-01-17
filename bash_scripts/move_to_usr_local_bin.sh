d6a73b3485d93e7b1b7804c21d323aede0a50d6d
Paul Statezny <paulstatezny@gmail.com>
2018-01-17 11:47:19 -0700

    Fix regexes for removing .sh from ends of files before creating aliases

--- bash_scripts/move_to_usr_local_bin.sh
+++ bash_scripts/move_to_usr_local_bin.sh
@@ -2,4 +2,4 @@
 
 DIR=`pwd`
 
-ls -1 | grep -v "README" | grep -v "move_to_usr_local_bin" | grep -v "go.sh" | sed 's/.sh//' | xargs -I % sh -c "sudo ln -s $DIR/%.sh /usr/local/bin/%"
+ls -1 | grep -v "README" | grep -v "move_to_usr_local_bin" | grep -v "go.sh" | sed 's/\.sh//' | xargs -I % sh -c "sudo ln -s $DIR/%.sh /usr/local/bin/%"
