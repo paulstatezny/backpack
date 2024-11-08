c261c7d43dd3e810b31ed40a024bbe29bed80dce
Paul Statezny <paul.statezny@river.com>
2024-11-08 13:32:01 -0700

    Update commit.sh

--- bash_scripts/commit.sh
+++ bash_scripts/commit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CURRENT_BRANCH=`git symbolic-ref --short HEAD`
-REGEX='^([0-9]+)'
+REGEX='BROK-([0-9]+)'
 
 if [[ $CURRENT_BRANCH =~ $REGEX ]] ; then
-  git commit -m "#${BASH_REMATCH[1]}: $1"
+  git commit -m "feat(BROK-${BASH_REMATCH[1]}): $1"
 else
   git commit -m "$1"
 fi
